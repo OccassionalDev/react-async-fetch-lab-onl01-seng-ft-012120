a7ea4d670f099b32ce755354d2778c9cd811c608
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-08 02:10:35 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 class App extends React.Component {
   componentDidMount() {
-    
+      fetch('the server URL', {
+    method: "POST",
+    headers: {
+      'Content-Type: application/json'
+    },
+    body: JSON.stringify(this.state)
+  })
   }
 }
 
