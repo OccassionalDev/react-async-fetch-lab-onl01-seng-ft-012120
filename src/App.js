cce153eb3624aae7800b80e883dd8ed9e1aee6df
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-08 02:10:55 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from 'react'
 class App extends React.Component {
   componentDidMount() {
       fetch('http://api.open-notify.org/astros.json', {
-    method: "POST",
-    headers: {
-      'Content-Type: application/json'
-    },
-    body: JSON.stringify(this.state)
-  })
+        method: "POST",
+        headers: {
+          'Content-Type: application/json'
+        },
+        body: JSON.stringify(this.state)
+      })
   }
 }
 
